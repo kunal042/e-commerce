803f77dc80895b75af8a9f774aa02f6a80d45121
kunal kumar <k1509kunal@gmail.com>
2024-05-16 22:47:29 +0530

    login logout page

--- shopproject/shopauth/urls.py
+++ shopproject/shopauth/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from shopauth import views
 
 urlpatterns = [
-    path("signup/",views.sign_up, name='signup')
+    path("signup/",views.sign_up, name='signup'),
+    path("login/",views.login, name='login')
 ]
--- shopproject/shopauth/views.py
+++ shopproject/shopauth/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+
+
+
 def sign_up(request):
     return render(request, 'auth/signup.html')
+
+def login(request):
+    return render(request, 'auth/login.html')
